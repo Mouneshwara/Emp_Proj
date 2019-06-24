bf1dd43a9a813a38a6a4af4672814534f5a415f2
Mouneshwara <mouneshwarakalal@gmail.com>
2019-06-24 22:33:51 +0530

    first commit

---
+++ src/main/java/com/example/demo/EmployeeTableApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTableApplication.class, args);
+	}
+
+}
